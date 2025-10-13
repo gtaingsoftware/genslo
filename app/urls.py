3a48b4029a81d6b92a30cd5002d76ac9514d704e
gtaingsoftware <gta.ing.software@gmail.com>
2025-10-13 20:13:40 -0300

    Update urls.py

--- app/urls.py
+++ app/urls.py
@@ -1,9 +1,7 @@
-from django.contrib import admin
-from django.urls import path, include
-from django.views.generic import TemplateView
+from django.urls import path
+from . import views
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("", TemplateView.as_view(template_name="index.html")),  # Página principal
-    path("", include("app.urls")), 
+    path("api/ejecutar/", views.ejecutar_programa, name="ejecutar_programa"),
 ]
+
