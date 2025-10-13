b7261824b839fe4f0d284ac43278201e0a47b826
gtaingsoftware <gta.ing.software@gmail.com>
2025-10-13 20:06:20 -0300

    Update urls.py

--- app/urls.py
+++ app/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("api/ejecutar/", views.ejecutar_programa, name="ejecutar_programa"),
+    path('api/generar-kml/', views.generar_kml, name='generar_kml'),
+    path('api/generar-txt/', views.generar_txt, name='generar_txt'),
 ]
